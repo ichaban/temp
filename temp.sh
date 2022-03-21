5752d67446a77ecc9776cc8abfc593ba953aa72f
Ihor Chaban <71515000+ichaban@users.noreply.github.com>
2022-03-21 18:38:51 +0200

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -6,9 +6,15 @@ echo "[client]" > /etc/zabbix/scripts/wsrep_config.cfg
 echo "user = \"$(grep 'mysql_user' /var/lib/zabbix/percona/scripts/ss_get_mysql_stats.php.cnf | cut -d"'" -f2)\"" >> /etc/zabbix/scripts/wsrep_config.cfg
 echo "password = \"$(grep 'mysql_pass' /var/lib/zabbix/percona/scripts/ss_get_mysql_stats.php.cnf | cut -d"'" -f2)\"" >> /etc/zabbix/scripts/wsrep_config.cfg
 
-curl -s https://pastebin.com/raw/1fuRC3KV | sed 's/\r$//' > /etc/zabbix/zabbix_agentd.d/wsrep_get.conf
-
 chown zabbix:zabbix /etc/zabbix/scripts/*
 chmod 755 /etc/zabbix/scripts/wsrep_get.sh
 
-systemctl restart zabbix-agent
+if which zabbix_agentd; then
+	curl -s https://pastebin.com/raw/1fuRC3KV | sed 's/\r$//' > /etc/zabbix/zabbix_agentd.d/wsrep_get.conf
+	systemctl restart zabbix-agent
+fi
+
+if which zabbix_agent2; then
+	curl -s https://pastebin.com/raw/1fuRC3KV | sed 's/\r$//' > /etc/zabbix/zabbix_agent2.d/wsrep_get.conf
+	systemctl restart zabbix-agent2
+fi 
